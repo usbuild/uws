2e9212e8eae7f7f451a8e497f73828d79e1b758e
Usbuild <njuzhangqichao@gmail.com>
2012-12-17 21:23:26 +0800

    add epoll for fcgi fd

--- uws_fastcgi.c
+++ uws_fastcgi.c
@@ -198,12 +198,12 @@ typedef struct {
 
 void 
 add_to_epoll(pConnInfo conn_info, uint32_t flag) {
-        struct epoll_event ev;
-        ev.events = flag | EPOLLET | EPOLLONESHOT;
-        conn_info->status = CS_UPSTREAM_READ;
-        ev.data.ptr = conn_info;
-        if(epoll_ctl(conn_info->epollfd, EPOLL_CTL_ADD, conn_info->serverfd, &ev) == -1)
-            exit_err("epoll_ctl");
+    struct epoll_event ev;
+    ev.events = flag | EPOLLET;
+    conn_info->status = CS_UPSTREAM_READ;
+    ev.data.ptr = conn_info;
+    if(epoll_ctl(conn_info->epollfd, EPOLL_CTL_ADD, conn_info->serverfd, &ev) == -1)
+        exit_err("epoll_ctl");
 }
 
 void
@@ -301,7 +301,7 @@ fastcgi_router(pConnInfo conn_info)
                 return;
             }
         }
-        
+
     }/*}}}*/
 
     fdata = (FCGI_LOCAL_DATA*) conn_info->ptr;
@@ -392,6 +392,7 @@ fastcgi_router(pConnInfo conn_info)
         conn_info->flag = 0x03;
     }/*}}}*/
 
+
     if(conn_info->flag  == 0x03) {
         for( ; ; ) {
             ssize_t res= write(conn_info->serverfd, fdata->smem->mem + fdata->mem_offset, fdata->smem->len - fdata->mem_offset);
@@ -411,71 +412,82 @@ fastcgi_router(pConnInfo conn_info)
         }
         fdata->mem_offset = 0;
         free_mem_t(fdata->smem);
+        conn_info->flag = 0x04;
+        
+        
+        struct epoll_event ev;
+        ev.events = EPOLLIN | EPOLLET;
+        conn_info->status = CS_UPSTREAM_READ;
+        ev.data.ptr = conn_info;
+        if(epoll_ctl(conn_info->epollfd, EPOLL_CTL_MOD, conn_info->serverfd, &ev) == -1)
+            exit_err("epoll_ctl");
     }
 
-    setblocking(conn_info->serverfd);
 
     //all_data_send!
     // if we have more content from fastcgi
-    bool more_content = read_response(conn_info->serverfd, fdata->smem);
+    if(conn_info->flag == 0x04) {
+        setblocking(conn_info->serverfd);
+        bool more_content = read_response(conn_info->serverfd, fdata->smem);
 
-    if(fdata->smem->len == 0) {
-        conn_info->status_code =  500;
-        apply_next_router(conn_info);
-        return;
-    }
-
-    char line[LINE_LEN] = {0};
-    unsigned char *oldpos = fdata->smem->mem;
-    unsigned char *pos;
-    struct http_header fcgi_response_header;
-    bzero(&fcgi_response_header, sizeof(fcgi_response_header));
-    char key[LINE_LEN];
-    char value[LINE_LEN];
-
-    char *time_string = get_time_string(NULL);
-    add_header_param("Server", UWS_SERVER, &fcgi_response_header);
-    add_header_param("Date", time_string, &fcgi_response_header);
-    uws_free(time_string);
-
-    while((pos = strstr(oldpos, "\r\n"))) {
-        if(oldpos == pos) break;
-        bzero(line, LINE_LEN);
-        strncpy(line, oldpos, pos - oldpos);
-        sscanf(line, "%[^:]: %s", key, value);
-        if(strcmp(key, "Status") == 0) {
-            fcgi_response_header.status_code = atoi(value);
-        } else {
-            push_header_param(key, value, &fcgi_response_header);
+        if(fdata->smem->len == 0) {
+            conn_info->status_code =  500;
+            apply_next_router(conn_info);
+            return;
         }
-        oldpos = pos + strlen("\r\n");
-    }
-    int content_len = fdata->smem->len - (pos - fdata->smem->mem) - strlen("\r\n");
 
-    char *str_len =  itoa(content_len);
-    add_header_param("Content-Length", str_len, &fcgi_response_header);
-    uws_free(str_len);
+        char line[LINE_LEN] = {0};
+        unsigned char *oldpos = fdata->smem->mem;
+        unsigned char *pos;
+        struct http_header fcgi_response_header;
+        bzero(&fcgi_response_header, sizeof(fcgi_response_header));
+        char key[LINE_LEN];
+        char value[LINE_LEN];
 
-    fcgi_response_header.http_ver = "HTTP/1.1";
-    if(fcgi_response_header.status_code == 0) fcgi_response_header.status_code = 200;
-    fcgi_response_header.status = get_by_code(fcgi_response_header.status_code);
+        char *time_string = get_time_string(NULL);
+        add_header_param("Server", UWS_SERVER, &fcgi_response_header);
+        add_header_param("Date", time_string, &fcgi_response_header);
+        uws_free(time_string);
 
-    char *header_str = str_response_header(&fcgi_response_header);
-    writen(conn_info->clientfd, header_str, strlen(header_str));
-    uws_free(header_str);
-    writen(conn_info->clientfd, pos, content_len + strlen("\r\n"));
+        while((pos = strstr(oldpos, "\r\n"))) {
+            if(oldpos == pos) break;
+            bzero(line, LINE_LEN);
+            strncpy(line, oldpos, pos - oldpos);
+            sscanf(line, "%[^:]: %s", key, value);
+            if(strcmp(key, "Status") == 0) {
+                fcgi_response_header.status_code = atoi(value);
+            } else {
+                push_header_param(key, value, &fcgi_response_header);
+            }
+            oldpos = pos + strlen("\r\n");
+        }
+        int content_len = fdata->smem->len - (pos - fdata->smem->mem) - strlen("\r\n");
 
-    while(more_content) {
+        char *str_len =  itoa(content_len);
+        add_header_param("Content-Length", str_len, &fcgi_response_header);
+        uws_free(str_len);
+
+        fcgi_response_header.http_ver = "HTTP/1.1";
+        if(fcgi_response_header.status_code == 0) fcgi_response_header.status_code = 200;
+        fcgi_response_header.status = get_by_code(fcgi_response_header.status_code);
+
+        char *header_str = str_response_header(&fcgi_response_header);
+        writen(conn_info->clientfd, header_str, strlen(header_str));
+        uws_free(header_str);
+        writen(conn_info->clientfd, pos, content_len + strlen("\r\n"));
+
+        while(more_content) {
+            free_mem_t(fdata->smem);
+            more_content = read_response(conn_info->serverfd, fdata->smem);
+            writen(conn_info->clientfd, fdata->smem->mem, fdata->smem->len);
+        }
+
+        free_header_params(&fcgi_response_header);
         free_mem_t(fdata->smem);
-        more_content = read_response(conn_info->serverfd, fdata->smem);
-        writen(conn_info->clientfd, fdata->smem->mem, fdata->smem->len);
+
+        uws_free(fdata);    
+        conn_info->ptr = NULL;
+        apply_next_router(conn_info);
     }
-
-    free_header_params(&fcgi_response_header);
-    free_mem_t(fdata->smem);
-
-    uws_free(fdata);    
-    conn_info->ptr = NULL;
-    apply_next_router(conn_info);
 }
 
